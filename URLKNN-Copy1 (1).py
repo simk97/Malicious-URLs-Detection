02867a903c76b5ddcbeb138e5dd62837b8f76a05
simk97 <42352278+simk97@users.noreply.github.com>
2019-05-22 20:01:35 +0530

    Add files via upload

---
+++ URLKNN-Copy1 (1).py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+allTokens = []
+def getTokens(input):
+    tokensBySlash = str(input).split('/')
+    allTokens = []
+    for i in tokensBySlash:
+        tokens = str(i).split('-')
+        tokensByDot = []
+        for j in range(0,len(tokens)):
+            tempTokens = str(tokens[j]).split('.')
+            tokensByDot = tokensByDot + tempTokens
+        allTokens = allTokens + tokens + tokensByDot
+        allTokens = list(set(allTokens))
+        if 'com' in allTokens:
+            allTokens.remove('com') 
+return allTokens
+
+
+# In[ ]:
+
+
+import pandas as pd
+import random
+import numpy as np
+from sklearn import preprocessing
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer()
+neigh = KNeighborsClassifier()
+def TL():
+    Data = 'http://web.archive.org/web/20181013193031/https://raw.githubusercontent.com/faizann24/Using-machine-learning-to-detect-malicious-URLs/master/data/data.csv'
+    queriescsv = pd.read_csv(Data,',',error_bad_lines=False)
+    queriesdf = pd.DataFrame(queriescsv)
+    queriesdata = np.array(queriesdf)
+    random.shuffle(queriesdata)
+    y = [d[1] for d in queriesdata]
+    corpus = [d[0] for d in queriesdata]
+    vectorizer = TfidfVectorizer(tokenizer=getTokens(queriesdata))
+    X = vectorizer.fit_transform(corpus) 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
+    k = 4  
+    neigh = KNeighborsClassifier(n_neighbors = k).fit(X_train,y_train)
+    print(neigh)
+    X_predict = ['wikipedia.com','google.com/search=faizanahad','pakistanifacebookforever.com/getpassword.php/','www.radsport-voggel.de/wp-admin/includes/log.exe','ahrenhei.without-transfer.ru/nethost.exe','www.itidea.it/centroesteticosothys/img/_notes/gum.exe'] #document.getElementById("input").value
+    X_predict = vectorizer.transform(X_predict)
+    y_Predict = neigh.predict(X_predict)
+    print(y_Predict)    
+    Trainaccuracy = metrics.accuracy_score(y_train, neigh.predict(X_train))
+    TestAccuracy = metrics.accuracy_score(y_test, y_Predict)
+return vectorizer, neigh, Trainaccuracy, TestAccuracy
+
+
+# In[ ]:
+
+
+TL()
+
+
+# In[ ]:
+
+
+
+
